b7fe12d5e04770a78a30c8b3cc5878aef151fc85
Sofien <sofienbktib@gmail.com>
2025-07-28 20:00:12 +0100

    fixed dto problem in the handler

--- src/eduflowbackend.Application/dtos/UserDto.cs
+++ src/eduflowbackend.Application/dtos/UserDto.cs
@@ -8,6 +8,6 @@ public class UserDto
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
-    public string Phone { get; set; }
+    public string PhoneNumber { get; set; }
     public Role Role { get; set; }
 }
