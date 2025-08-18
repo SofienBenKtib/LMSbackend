32960b666a917ebcd8c68c42ebb2d85277eb6764
Sofien <sofienbktib@gmail.com>
2025-08-18 01:42:12 +0100

    fixed file deletion logic

--- src/eduflowbackend.Application/Resources/Delete/DeleteResourceCommand.cs
+++ src/eduflowbackend.Application/Resources/Delete/DeleteResourceCommand.cs
@@ -3,12 +3,8 @@ using Mediator;
 
 namespace eduflowbackend.Application.Resources.Delete;
 
-public class DeleteResourceCommand : IRequest<Result<Guid>>
+public class DeleteResourceCommand : IRequest<Result>
 {
     public Guid ResourceId { get; set; }
-
-    public DeleteResourceCommand(Guid resourceId)
-    {
-        ResourceId = resourceId;
-    }
+    public DeleteResourceCommand(Guid resourceId) => ResourceId = resourceId;
 }
