ddc38bf5483095fc01150879ce96e52f3b6f021b
Sofien <sofienbktib@gmail.com>
2025-08-21 14:44:36 +0100

    added a new UpdateUserRequest class

---
+++ src/eduflowbackend.Application/Users/Update/UpdateUserRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eduflowbackend.Application.Users.Update;
+
+public class UpdateUserRequest
+{
+    public Guid Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    [EmailAddress]
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
+}
