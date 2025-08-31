896540d72d8486c920ee030967788a63eee65d35
Sofien <sofienbktib@gmail.com>
2025-08-31 02:16:34 +0100

    added delete session logic

--- src/eduflowbackend.Application/Sessions/Delete/DeleteSessionCommand.cs
+++ src/eduflowbackend.Application/Sessions/Delete/DeleteSessionCommand.cs
@@ -3,12 +3,4 @@ using Mediator;
 
 namespace eduflowbackend.Application.Sessions.Delete;
 
-public class DeleteSessionCommand : IRequest<Result<Guid>>
-{
-    public DeleteSessionCommand(Guid sessionId)
-    {
-        SessionId = sessionId;
-    }
-    
-    public Guid SessionId { get; set; }
-}
+public record DeleteSessionCommand(Guid Id) : IRequest<bool>;
--- src/eduflowbackend.Application/Sessions/Delete/DeleteSessionHandler.cs
+++ src/eduflowbackend.Application/Sessions/Delete/DeleteSessionHandler.cs
@@ -5,7 +5,7 @@ using Mediator;
 
 namespace eduflowbackend.Application.Sessions.Delete;
 
-public class DeleteSessionHandler : IRequestHandler<DeleteSessionCommand, Result<Guid>>
+public class DeleteSessionHandler : IRequestHandler<DeleteSessionCommand, bool>
 {
     private readonly IRepository<Session> _repository;
 
@@ -14,14 +14,8 @@ public class DeleteSessionHandler : IRequestHandler<DeleteSessionCommand, Result
         _repository = repository;
     }
 
-    public async ValueTask<Result<Guid>> Handle(DeleteSessionCommand request, CancellationToken cancellationToken)
+    public async ValueTask<bool> Handle(DeleteSessionCommand request, CancellationToken cancellationToken)
     {
-        //  Finding the session in the DB
-        var session = await _repository.GetByIdAsync(request.SessionId);
-
-
-        //  Deleting the session
-        await _repository.DeleteAsync(session);
-        return Result.Ok();
+        return SessionStorage.Sessions.TryRemove(request.Id, out _);
     }
 }
